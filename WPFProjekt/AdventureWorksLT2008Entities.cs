48c11fca4d0832553aa90ab1bc7ce272af2817f1
xxMateusz <mateusz.bochenek@microsoft.wsei.edu.pl>
2023-04-06 15:34:01 +0200

    Dodaj pliki projektów.

---
+++ WPFProjekt/AdventureWorksLT2008Entities.cs
@@ -0,0 +1,7 @@
+﻿namespace WPFProjekt
+{
+    internal class AdventureWorksLT2008Entities
+    {
+        public object Products { get; internal set; }
+    }
+}
---
+++ WPFProjekt/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFProjekt
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //window2 win2 = new window2();
+            //win2.Show();
+            Window1 window1 = new Window1();
+            window1.Show();
+        }
+    }
+
+     public class window2
+    {
+    }
+}
